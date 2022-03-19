5ae08d6a6e11a99f41e8562138705f8ffe97e735
Nithyadevi <nithi2218@gmail.com>
2022-03-19 23:24:07 +1100

    Assignment Completed

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code here!
+main.remove(main.querySelector('main'));
+
+const newHeader = document.createElement('h1')
+newHeader.id = 'victory'
+newHeader.innerHTML = "Nithi is the champion"
